ffd05715b5787776368eadfd52a4f6d90c7d5f53
Jonathan Wight <jwight@mac.com>
2025-01-04 16:37:25 -0800

    Initial Commit

---
+++ FunctionKeyToggle/FunctionKeyToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionKeyToggleApp.swift
+//  FunctionKeyToggle
+//
+//  Created by Jonathan Wight on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FunctionKeyToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
